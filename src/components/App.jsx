0ea3294e0df5163e19d20aa22316bfcaf765f580
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-11-22 16:20:50 +0630

    starter files

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Players from './Players';
+import Teams from './Teams';
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/players' element={<Players />} />
+        <Route path='/teams' element={<Teams />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ src/components/Teams.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Teams() {
+  return <div className='container'>Teams</div>;
+}
